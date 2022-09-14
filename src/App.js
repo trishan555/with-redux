851cf823ab880721da4521424d2da80434253689
TrishLeaf <trishanp96@gmail.com>
2022-09-14 15:41:15 +0530

    added userSlice

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='addUser'>
+                <input type='text' placeholder='Name...' />
+                <input type='text' placeholder='Username...' />
+                <button>Add User</button>
+            </div>
+            <div className='displayUsers'>
+                {userList.map((user) => {
+                    return (
+                        <div>
+                            <h1> {user.name}</h1>
+                            <h1> {user.username}</h1>
+                            <input type='text' placeholder='New Username...' />
+                            <button>Update Username</button>
+                            <button>Delete User</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
